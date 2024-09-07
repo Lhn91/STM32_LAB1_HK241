42ff4fcf21eed58a44586f330816867c3f1ea380
Lhn91 <nguyen.le1909@hcmut.edu.vn>
2024-09-07 17:07:34 +0700

    Ex1

--- STM32PROJECT/Core/Src/ex1.c
+++ STM32PROJECT/Core/Src/ex1.c
@@ -5,12 +5,31 @@
  *      Author: 84859
  */
 #include "ex1.h"
-int led_status = LED_OFF;
+
+void Led_on(){
+	HAL_GPIO_WritePin(Led_red_GPIO_Port, Led_red_Pin, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(Led_yellow_GPIO_Port, Led_yellow_Pin, GPIO_PIN_SET);
+}
+void Led_off(){
+	HAL_GPIO_WritePin(Led_red_GPIO_Port, Led_red_Pin, GPIO_PIN_SET);
+	HAL_GPIO_WritePin(Led_yellow_GPIO_Port, Led_yellow_Pin, GPIO_PIN_RESET);
+}
 void init_exercise1(){
 
 }
-
+int Led_status = LED_OFF;
+int count1 = 2;
 void exercise1_run(){
-
+	if(Led_status == LED_OFF && count1==0){
+		Led_on();
+		Led_status = LED_ON;
+		count1 = 2;
+	}
+	if(Led_status == LED_ON && count1 == 0){
+		Led_off();
+		Led_status = LED_OFF;
+		count1 = 2;
+	}
+	count1--;
 }
 
