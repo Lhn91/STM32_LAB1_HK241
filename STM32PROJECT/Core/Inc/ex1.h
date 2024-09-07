9972be6f96ef19977436e519ff10ab1cfde06272
Lhn91 <nguyen.le1909@hcmut.edu.vn>
2024-09-07 16:30:46 +0700

    init_stm32_lab1

---
+++ STM32PROJECT/Core/Inc/ex1.h
@@ -0,0 +1,18 @@
+/*
+ * ex1.h
+ *
+ *  Created on: Sep 7, 2024
+ *      Author: 84859
+ */
+
+#ifndef INC_EX1_H_
+#define INC_EX1_H_
+#include "main.h"
+
+#define LED_ON 	1
+#define LED_OFF	0
+
+void init_exercise1();
+void exercise1_run();
+
+#endif /* INC_EX1_H_ */
---
+++ STM32PROJECT/Core/Src/ex1.c
@@ -0,0 +1,16 @@
+/*
+ * ex1.c
+ *
+ *  Created on: Sep 7, 2024
+ *      Author: 84859
+ */
+#include "ex1.h"
+int led_status = LED_OFF;
+void init_exercise1(){
+
+}
+
+void exercise1_run(){
+
+}
+
